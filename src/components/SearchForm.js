a40ab71670c48f9710f303f0787490800a39abfd
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2023-07-10 11:22:08 +0530

    files upload

---
+++ src/components/SearchForm.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const {setSearchTerm}=useGlobalContext();
+  const searchValue=React.useRef('');
+
+  React.useEffect(()=>{
+    searchValue.current.focus()
+  },[])
+ 
+  const searchCocktail=()=>{
+    setSearchTerm(searchValue.current.value)
+  }
+
+  const handleSubmit=((e)=>{
+    e.preventDefault();
+  })
+
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor="name">Search Your Favourite Cocktail</label>
+        </div>
+        <input type='text' id='name' ref={searchValue} onChange={searchCocktail}/>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
